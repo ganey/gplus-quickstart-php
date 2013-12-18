0440be2755103abedb80552cafc1954e305a9089
ganey <github@ganey.co.uk>
2013-12-18 20:22:22 +0000

    Update and rename index.html to index.php
    removed most default information, set to include new google_login.php page to handle login information

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+//testcontent
+require_once 'google_login.php';<!--
+
+?>
+<!--
+/*
+ * Original master is https://github.com/googleplus/gplus-quickstart-php
+ * Forked by Ganey on 18th Dec 2013
+ *
+ * Original Copyright Information is as follows:
+ *
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+-->
+
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel='stylesheet' href='style.css' />
+</head>
+<body>
+<header><h1>Google+ PHP Data</h1></header>
+
+</div>
+</body>
+</html>
