fea65dfa24465162edaaf2eb336fea8899a5f4ef
ganey <github@ganey.co.uk>
2013-12-18 20:39:05 +0000

    Create google_config.php

---
+++ google_config.php
@@ -0,0 +1,19 @@
+<?php
+//google config
+
+const CLIENT_ID = 'YOUR_CLIENT_ID';
+
+/**
+ * Replace this with the client secret you got from the Google APIs console.
+ */
+const CLIENT_SECRET = 'YOUR_CLIENT_SECRET';
+
+/**
+ * Optionally replace this with your application's name.
+ */
+const APPLICATION_NAME = "Google+ PHP Data Quickstart";
+
+/**
+ * Optionally replace this with your developer key.
+ */
+const DEVELOPER_KEY = 'YOUR_DEVELOPER_KEY';
